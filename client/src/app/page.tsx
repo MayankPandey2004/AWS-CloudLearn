63644c2254635444977395ecc7db155e74b770bf
MayankPandey2004 <mpandey2004@gmail.com>
2025-05-22 15:28:26 +0530

    initial

---
+++ client/src/app/page.tsx
@@ -0,0 +1,14 @@
+import NonDashboardNavBar from "@/components/NonDashboardNavBar"
+import Landing from "./(nondashboard)/landing/page";
+import Footer from "@/components/Footer"
+export default function Home() {
+  return (
+    <div className="nondashboard-layout">
+      <NonDashboardNavBar/>
+      <main className="nondashboard-layout__main">
+        <Landing/>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>&copy; 2025 Mayank Pandey. All Rights Reserved</p>
+            <div className='footer__links'>
+                {["About", "Privacy", "Policy", "Contact"].map((item) => (
+                    <Link
+                        key={item}
+                        href={`/${item.toLowerCase().replace(" ", "-")}`}
+                        className="footer__link">
+                        {item}
+                        scroll={false}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Footer
