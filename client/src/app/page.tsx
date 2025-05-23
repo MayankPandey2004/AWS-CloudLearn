e23762cc39d146a3e2459006d09627951052eb67
Mayank Pandey <139049123+MayankPandey2004@users.noreply.github.com>
2025-05-24 03:38:19 +0530

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,23 @@
-import NonDashboardNavBar from "@/components/NonDashboardNavBar"
+import Head from "next/head";
+import NonDashboardNavBar from "@/components/NonDashboardNavBar";
 import Landing from "./(nondashboard)/landing/page";
-import Footer from "@/components/Footer"
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
-    <div className="nondashboard-layout">
-      <NonDashboardNavBar/>
-      <main className="nondashboard-layout__main">
-        <Landing/>
-      </main>
-      <Footer/>
-    </div>
+    <>
+      <Head>
+        <title>My Custom Home Page</title>
+        <meta name="description" content="This is the landing page of my awesome site." />
+      </Head>
+
+      <div className="nondashboard-layout">
+        <NonDashboardNavBar />
+        <main className="nondashboard-layout__main">
+          <Landing />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
